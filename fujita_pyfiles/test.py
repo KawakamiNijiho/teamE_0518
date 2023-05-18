b2186619fccecf74f6dd29fade02606fa85111e3
ItsukiFujitads <fujita.itsuki.sw@mynavi.jp>
2023-05-18 08:06:51 +0000

    test change

--- fujita_pyfiles/test.py
+++ fujita_pyfiles/test.py
@@ -5,7 +5,7 @@ nl=int(args[2])
 eng=int(args[3])
 sum=math+nl+eng
 bool_1=((math >= 70) and (nl >= 70) and (eng >=70))or(sum>=220)
-bool_2=(((math >= 50) and nl >= 50) and (eng >= 50))
+bool_2=((math >= 50) and (nl >= 50) and (eng >= 50))
 if bool_1 and bool_2:
     
     print("合格",end="")
