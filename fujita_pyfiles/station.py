9452837c59cce498075052ce03d1e1db240aee73
ItsukiFujitads <fujita.itsuki.sw@mynavi.jp>
2023-05-19 08:17:47 +0000

    distance try

--- fujita_pyfiles/station.py
+++ fujita_pyfiles/station.py
@@ -6,11 +6,15 @@ args=sys.argv
 station_1=str(args[1])
 station_2=str(args[2])
 
-#辞書定義
-dist_dict={"東京":0.00,"品川":6.78,"新横浜":25.54,"名古屋":342.02,"京都":476.31,"新大阪":515.35}
+try:
 
-#距離計算
-dist=round(abs(dist_dict[station_2]-dist_dict[station_1]),2)
+    #辞書定義
+    dist_dict={"東京":0.00,"品川":6.78,"新横浜":25.54,"名古屋":342.02,"京都":476.31,"新大阪":515.35}
 
-#出力
-print(dist,end="")
+    #距離計算
+    dist=round(abs(dist_dict[station_2]-dist_dict[station_1]),2)
+
+    #出力
+    print(dist,end="")
+except:
+    print("のぞみの停車駅を引数に設定してください",end="")
