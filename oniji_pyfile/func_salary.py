70159ff7ca02a47f66131293345306d069cf6fd9
KawakamiNijiho <kawakami.nijiho.qm@mynavi.jp>
2023-05-24 01:39:34 +0000

    支払額の計算

---
+++ oniji_pyfile/func_salary.py
@@ -0,0 +1,14 @@
+def calcsalary(salary):
+    if(salary <= 1000000):
+     tax = salary * 0.1
+    else:
+     tax = 1000000 * 0.1 + (salary-1000000) * 0.2
+
+    from decimal import Decimal, ROUND_HALF_UP
+    tax = Decimal(str(tax)).quantize(Decimal("0"), rounding=ROUND_HALF_UP)
+    
+    pay = int(salary - tax)
+
+    
+
+    print(f"給与：{salary}、支給額：{pay}、税額{tax}")
