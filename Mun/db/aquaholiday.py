489b87040cb43a6f308794550f5a144df1a64125
slo-oww <mun.seongcheol.qf@mynavi.jp>
2023-05-25 02:34:06 +0000

    update files

--- Mun/db/aquaholiday.py
+++ Mun/db/aquaholiday.py
@@ -1,36 +1,34 @@
-from datetime import date,sys
-from database import session
+import sys
+from datetime import date
+from database import session 
 from tables import Holiday
 
-args = sys.argv
+# コマンドライン引数の分割代入
+dt, adult, child  = sys.argv[1:]
+# datetime型に変換
+dt = date(int(dt[:4]),int(dt[4:6]),int(dt[6:8]))
+# 入力された日付の曜日を算出
+weekday = dt.strftime("%a")
 
+adult_ticket = 2000
+child_ticket = 1200
+sum_ticket = 0
 
-year = int(args[1][0:4])
-month = int(args[1][4:6])
-day = int(args[1][6:8])
-
-dt = date(year, month, day)
-
-holidays = session.query(Holiday.holi_date).filter_by(holi_date=dt).all()
-
-print(holidays)
-
-if holidays is not None:
-    print("Today is holiday")
+if weekday == "Sat" or weekday == "Sun":
+    # 休日は料金を更新する
+    adult_ticket = 2400
+    child_ticket = 1500
+    
 else:
-    print("Today is Workday")
-
-# weekday = dt.strftime("%a")
-
-# #old[0] = 平日 old[1] = 週末
-# old = (2000,2400) 
-# young = (1200,1500)
-
-
-# #args[2] = 大人 args[3] = 子供
-# if weekday == "Sat" or weekday == "Sun":
-#     sum = (int(args[2])*old[1]) + (int(args[3])*young[1])
-# else:
-#     sum = (int(args[2])*old[0]) + (int(args[3])*young[0])
-
-# print(sum,end="")
+    # 休日でなければholidayDBを取得
+    holiday = session.query(Holiday.holi_date).filter_by(holi_date=dt).first()
+    if holiday is None:
+        pass
+    else :
+        # 祝日の処理
+        adult_ticket = 2400
+        child_ticket = 1500
+        
+# 料金の計算
+sum_ticket = adult_ticket * int(adult) + child_ticket * int(child)
+print(sum_ticket,end="")
